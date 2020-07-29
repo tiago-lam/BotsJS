582a8bf86dcb462a32863d73cc56ef7ba280463f
Tiago-LA-Machado <tiago.machado@nyu.edu>
2020-07-29 16:38:35 -0400

    function to parse project

--- temp/getFile.js
+++ temp/getFile.js
@@ -1,20 +1,24 @@
 var xmlhttp = new XMLHttpRequest();
 var user = "user=" + "sepulveda.f"
-var title = "title=" + "Playable%20Scenarios"
+var title = "title=" + "Playable Scenarios"
 xmlhttp.onreadystatechange = function() {
 
     if (this.readyState == 4 && this.status == 200) {
 
+    	console.log(this.responseText);
         var obj = this.responseText;
         console.log(obj);
     }
     //startSpriteListObserver();
 };
-/**
- * Prepare and send the GET request to the server
- */
-
-
 
 xmlhttp.open("GET", "https://studycrafter.com/db/request.php?" + user + "&" + title, true);
-xmlhttp.send();
+
+
+try{
+    xmlhttp.send();
+}catch(err){
+    if(e.toString().startsWith("NetworkError")){
+        console.log(e.toString())
+    }
+}
