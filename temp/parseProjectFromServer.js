fa8ade0301b3d43d9f33d8e9aa6462f46e6e8eaa
Tiago-LA-Machado <tiago.machado@nyu.edu>
2020-07-29 17:36:05 -0400

    parsing script

---
+++ temp/parseProjectFromServer.js
@@ -0,0 +1,31 @@
+function parseProjectFromServer(project)
+{
+	var projectObj = {};
+	var projectArr = project.split("!!!!!!")
+	projectObj['project_id'] = projectArr[0]
+	projectObj['project_title'] = projectArr[1]
+	projectObj['user_name'] = projectArr[2]
+	var projectJson = projectArr[3].split("~~~~~~~~")
+	projectObj['project_json'] = projectJson[0]
+
+	var projectScenes = []
+	var sceneObj = {}
+	sceneObj['scene_id'] = projectJson[1]
+	sceneObj['scene_title'] = projectArr[4]
+	var sceneAndNextId = projectArr[6].split("~~~~~~~~")
+	sceneObj['scene_json'] = sceneAndNextId[0]
+	projectScenes.push(sceneObj)
+	
+	for(var idx = 7; idx < projectArr.length; idx+=3)
+	{
+		sceneObj = {}
+		sceneObj['scene_id'] = sceneAndNextId[1]
+		sceneObj['scene_title'] = projectArr[idx]
+		sceneAndNextId = projectArr[idx+2].split("~~~~~~~~")
+		sceneObj['scene_json'] = sceneAndNextId[0]
+		projectScenes.push(sceneObj)	
+	}
+
+	projectObj['scenes'] = projectScenes
+	return projectObj;
+}
