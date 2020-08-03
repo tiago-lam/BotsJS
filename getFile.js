5134f7b33b27f1c5c5b735a2b03ca156220585b2
Tiago-LA-Machado <tiago.machado@nyu.edu>
2020-08-03 18:23:20 -0400

    changing in graph structure generation (4)

--- getFile.js
+++ getFile.js
@@ -73,7 +73,7 @@ function parseScenes(scns)
     for(var i = 0; i < scns.length; i++)
     {
         var scene = scns[i]["scene_json"]
-        scene = scene.replace(/index":"/g,'idx":"')
+        //scene = scene.replace(/index":"/g,'idx":"')
         scene = JSON.parse(scene)
         createStartNodes(scene)
         scns[i] = scene
