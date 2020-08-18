5f79906003881128d197e3bb49b599af67081c5b
Tiago-LA-Machado <tiago.machado@nyu.edu>
2020-08-18 14:39:16 -0400

    transparent edge(9)

--- screen.js
+++ screen.js
@@ -187,7 +187,7 @@ function drawNodeWithColor(d, c)
 
 function drawLink(l)
 {
-	if(!scenePar.includes(parseInt(l.sceneGroup))
+	if(!scenePar.includes(parseInt(l.sceneGroup)))
 		ctx.strokeStyle = "rgba(23, 23, 23," + alphaPar + ")";
 	else
 		ctx.strokeStyle = "#0000FE";
