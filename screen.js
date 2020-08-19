05c6c1ac87221bf870807af9458dd0dc812aa9e8
Tiago-LA-Machado <tiago.machado@nyu.edu>
2020-08-18 20:31:18 -0400

    fixing pan and drag

--- screen.js
+++ screen.js
@@ -122,27 +122,27 @@ function dragsubject()
 {
 	// if(dragScreen)
 	// {
-    	var i,
-	    x = transform.invertX(d3.event.x),
-	    y = transform.invertY(d3.event.y),
-	    dx,
-	    dy;
-	    for (i = nodes.length - 1; i >= 0; --i) {
-	      node = nodes[i];
-	      dx = x - node.x;
-	      dy = y - node.y;
+  //   	var i,
+	 //    x = transform.invertX(d3.event.x),
+	 //    y = transform.invertY(d3.event.y),
+	 //    dx,
+	 //    dy;
+	 //    for (i = nodes.length - 1; i >= 0; --i) {
+	 //      node = nodes[i];
+	 //      dx = x - node.x;
+	 //      dy = y - node.y;
 
-	      if (dx * dx + dy * dy < 20 * 20) {
+	 //      if (dx * dx + dy * dy < 20 * 20) {
 
-	        node.x = transform.applyX(node.x);
-	        node.y = transform.applyY(node.y);
+	 //        node.x = transform.applyX(node.x);
+	 //        node.y = transform.applyY(node.y);
 
-	        return node;
-	      }
-		}
+	 //        return node;
+	 //      }
+		// }
 	//}	
     // else
-    // 	return simulation.find(d3.event.x, d3.event.y);
+    return simulation.find(d3.event.x, d3.event.y);
 }
 
 function drawNodeBasedOnScene(d, sceneID, alpha)
